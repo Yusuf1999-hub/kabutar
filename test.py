2ecc184ffeb1e43348d957d133adaec8ad888ad5
Yusuf <yusufkhusanovic@gmail.com>
2026-04-03 16:23:05 +0500

    ! qop ketganakan uzuuur

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ print("HATO ")
 print("EXAM")
 
 
-# git branch -> mavjud branchlar ro'yxatini ko'rsatadi 
-# git branch testcha -> yangi branch yaratadi 
-# git checkout testcha -> ko'rsatilgan branchga o'tqazib beradi 
-# git checkout -b karam -> ko'rsatilgan branchni yaratib bir vaqtda ichiga kirib beradi
+# git branch -> mavjud branchlar ro'yxatini ko'rsatadi !
+# git branch testcha -> yangi branch yaratadi !
+# git checkout testcha -> ko'rsatilgan branchga o'tqazib beradi !
+# git checkout -b karam -> ko'rsatilgan branchni yaratib bir vaqtda ichiga kirib beradi!
