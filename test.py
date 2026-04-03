d69a8bb9edc757aed94f961f5f00e40cd122264e
Yusuf <yusufkhusanovic@gmail.com>
2026-04-03 16:20:58 +0500

    Koment yozdim

--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 print("KALXo'z")
 print("HATO ")
 print("EXAM")
+
+
+# git branch -> mavjud branchlar ro'yxatini ko'rsatadi 
+# git branch testcha -> yangi branch yaratadi 
+# git checkout testcha -> ko'rsatilgan branchga o'tqazib beradi 
+# git checkout -b karam -> ko'rsatilgan branchni yaratib bir vaqtda ichiga kirib beradi
